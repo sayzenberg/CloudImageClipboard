fb0137973f539dbf5e9ded97281487ee3c48c1c0
Stanley Ayzenberg <sayz93@gmail.com>
2018-07-23 15:55:10 -0700

    Added basic support for OAuth

--- src/iOS/CloudImageClipboard/ViewController.swift
+++ src/iOS/CloudImageClipboard/ViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import OAuthSwift
 
 class ViewController: UIViewController {
-
+    //MARK: Properties
+    var oauthswift: OAuth2Swift?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,33 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //MARK: Actions
+    
+    @IBAction func signIn(_ sender: UIButton) {
+        oauthswift = OAuth2Swift(
+            consumerKey: "03890202-e68d-4781-910a-586a45e29df0",
+            consumerSecret: "",
+            authorizeUrl:   "https://login.microsoftonline.com/common/oauth2/v2.0/authorize",
+            accessTokenUrl: "https://login.microsoftonline.com/common/oauth2/v2.0/token",
+            responseType:   "code"
+        )
+        
+        let _ = oauthswift?.authorize(
+            withCallbackURL: URL(string: "msal03890202-e68d-4781-910a-586a45e29df0://auth")!,
+            scope: "UserActivity.ReadWrite.CreatedByApp",
+            state: generateState(withLength: 32),
+            success: { credential, response, parameters in
+                print(credential.oauthToken)
+                let alert = UIAlertController(title: "Got a token!", message: credential.oauthToken, preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Close", style: .cancel, handler: nil))
+                self.present(alert, animated: true)
+                // Do your request
+            },
+            failure: { error in
+                print(error.localizedDescription)
+            })
 
+    }
+    
 }
 
---
+++ src/iOS/Pods/Target Support Files/Pods-CloudImageClipboard/Pods-CloudImageClipboard-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CloudImageClipboard : NSObject
+@end
+@implementation PodsDummy_Pods_CloudImageClipboard
+@end
