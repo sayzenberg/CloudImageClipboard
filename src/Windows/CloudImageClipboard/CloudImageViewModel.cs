eca7b724dc40b628d7bb70db3833a3f9b23ad934
Stanley Ayzenberg <stanay@microsoft.com>
2018-07-25 00:11:25 -0700

    Implemented UWP

---
+++ src/Windows/CloudImageClipboard/CloudImageViewModel.cs
@@ -0,0 +1,40 @@
+﻿namespace CloudImageClipboard
+{
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+    using Annotations;
+
+    class CloudImageViewModel: INotifyPropertyChanged
+    {
+        private string imageUrl;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string ImageUrl
+        {
+            get
+            {
+                return imageUrl;
+            }
+            set
+            {
+                if (!string.Equals(value, imageUrl))
+                {
+                    imageUrl = value;
+                    OnPropertyChanged(nameof(ImageUrl));
+                }
+            }
+        }
+
+        public CloudImageViewModel(string imageUrl)
+        {
+            this.imageUrl = imageUrl;
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
